e7195d6a58ac5b4e4d180fda820fd99033a9eff0
Hexscript101 <github.crushing313@passinbox.com>
2026-06-05 17:55:58 +0200

    base clean
    Base neutra dalle implementazioni di protocol

--- version1/functions.c
+++ version1/functions.c
@@ -365,18 +365,11 @@ int set_up_client(int fd, int port, char address[])
 
     otherPlayer.sin_family = AF_INET;
     otherPlayer.sin_port = htons(port);
-    inet_pton(AF_INET, address, &otherPlayer.sin_addr);
+    inet_pton(AF_INET, address, &otherPlayer.sin_addr );
 
     res = connect(fd, (struct sockaddr *)&otherPlayer, sizeof(otherPlayer));
 
-    if (res < 0)
-    {
-       flag = FAIL;
-    }else
-    {
-        flag = OK;
-    }
-    return flag;
+    return res;
 }
 
 
